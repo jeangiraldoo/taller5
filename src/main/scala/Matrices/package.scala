0cba9560adbe21cdc094f0e26edeb952671bd0cd
Jean Giraldo <jean.giraldoo@gmail.com>
2025-05-06 23:58:51 -0500

    feat: implement multMatrizParalela

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -1,4 +1,7 @@
 import scala.util.Random
+import common._
+
+import java.util.concurrent.ForkJoinTask
 
 package object Matrices {
 
@@ -20,10 +23,26 @@ package object Matrices {
     (v1 zip v2).map({case(i,j) => (i*j)}).sum
   }
 
+  // Multiplicación estándar de matrices
+
   def multMatriz(m1:Matriz, m2:Matriz): Matriz = {
     val m2T = transpuesta(m2)
     val n = m1.length
     Vector.tabulate(n, n)((i, j) => prodPunto(m1(i),m2T(j)))
+  }
+
+  def multMatrizParalela(m1:Matriz, m2:Matriz): Matriz = {
+    val m2T = transpuesta(m2)
+    val n = m1.length
+
+    val tareas: Vector[ForkJoinTask[Vector[Int]]] = Vector.tabulate(n) { i =>
+      task {
+        Vector.tabulate(n) { j =>
+          prodPunto(m1(i), m2T(j))
+        }
+      }
+    }
+    tareas.map(_.join())
   }
 
   def subMatriz(m:Matriz, i:Int, j:Int, l:Int):Matriz = {
